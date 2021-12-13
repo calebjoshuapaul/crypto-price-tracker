2a2b4d24bdfdf30c72689fc68fdc36dc28e1a403
Caleb Joshua <43238940+calebjoshuapaul@users.noreply.github.com>
2021-12-13 19:00:27 +0530

    update

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const tableBody = document.querySelector(".tableBody");
 
-const data = fetch(
-  "https://crypto-price-tracker-worker.calebjoshuapaul.workers.dev/"
-)
-  .then((res) => res.json)
-  .then((data) => data);
+async function fetchData() {
+  const data = await fetch(
+    "https://crypto-price-tracker-worker.calebjoshuapaul.workers.dev/"
+  )
+    .then((response) => response.json())
+    .then((data) => data);
 
-for (let i = 0; i < data.length; i++) {
-  tableBody.innerHTML = `
-    <tr>
+  console.log(data);
+
+  for (let i = 0; i < data.length; i++) {
+    let tr = document.createElement("tr");
+    tr.innerHTML = `
         <td>${data[i].symbol} - ${data[i].name}</td>
-        <td>${data[i].price.toFixed(2)}</td>
-    </tr>
-    `;
+        <td>₹${data[i].price.toFixed(2)}</td>
+        `;
+    tableBody.append(tr);
+  }
 }
+
+fetchData();
